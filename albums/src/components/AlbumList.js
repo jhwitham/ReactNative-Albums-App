dd43c264341b769cde04c959b68eb1547f3fa394
John Herman Witham <john.h.witham@gmail.com>
2017-05-26 21:44:20 +0300

    set albums this.setState to http response.data

--- albums/src/components/AlbumList.js
+++ albums/src/components/AlbumList.js
@@ -7,7 +7,7 @@ class AlbumList extends Component {
 
   componentWillMount() {
     axios.get('https://rallycoding.herokuapp.com/api/music_albums')
-      .then(response => console.log(response));
+      .then(response => this.setState({ albums: response.data }));
   }
 
   render() {
